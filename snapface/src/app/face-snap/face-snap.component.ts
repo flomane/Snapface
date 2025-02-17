f9a668998715e689aa3d1fbc5aeac3ce44f60e4a
barry <abdoulsebory@gmail.com>
2025-02-18 00:25:43 +0100

    ajout des snaps

--- snapface/src/app/face-snap/face-snap.component.ts
+++ snapface/src/app/face-snap/face-snap.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-face-snap',
@@ -6,6 +6,23 @@ import { Component } from '@angular/core';
   templateUrl: './face-snap.component.html',
   styleUrl: './face-snap.component.scss'
 })
-export class FaceSnapComponent {
+export class FaceSnapComponent implements OnInit {
+     title: string; 
+     description: string;
+     createdAt: Date;
+     snaps: number;
+     imageUrl: string;
 
+ constructor(){}
+ ngOnInit(){
+  this.title = 'Archibald';
+  this.description = 'Mon meilleur ami  depuis toujours';
+  this.createdAt = new Date();
+  this.snaps = 0;
+  this.imageUrl = 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg';
+ }
+ onAddSnap(): void{
+  this.snaps++;
+
+}
 }
